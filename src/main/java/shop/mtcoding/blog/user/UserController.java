5d5bb1460c5fb49c55cd64d87df2abdc6b3a7856
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-05 15:21:21 +0900

    UserController가 UserRepository 의존하기

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,11 +1,16 @@
 package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+
+    // 자바의 final 변수는 반드시 초기화가 되어야 한다.
+    private final UserRepository userRepository;
 
     @PostMapping("/join")
     public String join(UserRequest.joinDTO requestDTO){
