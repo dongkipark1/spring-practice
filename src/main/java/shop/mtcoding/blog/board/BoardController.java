2927d2234e06664cb2740599eefa70292f329f1d
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-05 17:13:29 +0900

    board 컬렉션 조회

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,11 +1,13 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class BoardController {
-
     @GetMapping({ "/", "/board" })
     public String index() {
         return "index";
