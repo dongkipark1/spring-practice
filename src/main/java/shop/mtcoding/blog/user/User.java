53da1b3a85e248016ce34c924f0e557595a1e968
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-05 14:44:25 +0900

    user 엔티티만들기

---
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+// 서버를 열면 아직 테이블이 만들어지지 않았다
+// 왜 테이블 생성쿼리가 없는 거지? 테이블이 만들어 지지 않았다.
+@Table(name = "user_tb") // 테이블을 생성할 때 자바 객체로 만들 수 있다. 이때는 @Entity를 사용한다.
+@Data
+@Entity
+public class User {
+    @Id // PK 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 전략
+    private int id;
+    private String username;
+    private String password;
+
+    // 카멜 표기법으로 만들면 DB는 created_at으로 만들어진다. (언더스코어 기법)
+    private String email;
+
+}
+
+
