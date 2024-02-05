44ce94f3448c40c78becbb0f84a4c5d8613614e2
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-05 15:44:52 +0900

    login page 확인

--- src/main/java/shop/mtcoding/blog/user/User.java
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -3,6 +3,8 @@ package shop.mtcoding.blog.user;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 // 서버를 열면 아직 테이블이 만들어지지 않았다
 // 왜 테이블 생성쿼리가 없는 거지? 테이블이 만들어 지지 않았다.
 @Table(name = "user_tb") // 테이블을 생성할 때 자바 객체로 만들 수 있다. 이때는 @Entity를 사용한다.
@@ -14,9 +16,9 @@ public class User {
     private int id;
     private String username;
     private String password;
-
-    // 카멜 표기법으로 만들면 DB는 created_at으로 만들어진다. (언더스코어 기법)
     private String email;
+    // 카멜 표기법으로 만들면 DB는 created_at으로 만들어진다. (언더스코어 기법)
+    private LocalDateTime createdAt;
 
 }
 
