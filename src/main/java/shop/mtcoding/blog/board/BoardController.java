bab271cd89d86f55490c582f79f20276a87872e8
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-02-06 10:44:33 +0900

    게시글 쓰기 로그인 체크

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,10 +1,12 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -12,6 +14,7 @@ import java.util.List;
 @Controller
 public class BoardController {
 
+    private final HttpSession session;
     private final BoardRepostiory boardRepostiory;
     @GetMapping({ "/", "/board" })
     public String index(HttpServletRequest request) {
@@ -21,10 +24,20 @@ public class BoardController {
 
         return "index";
     }
-
+        // /board/saveForm 요청(get)이 온다.
     @GetMapping("/board/saveForm")
     public String saveForm() {
-        return "board/saveForm";
+        //session 영역에 sessionUser 키값에 user 객체 있는지 체크
+       User sessionUser = (User) session.getAttribute("sessionUser");
+
+        // 값이 null 이면 login 페이지로 리다이렉션
+        // 값이 null 이 아니면 /board/saveForm으로 이동
+
+        if (sessionUser == null){
+            return "redirect:/loginForm";
+        }else {
+            return "board/saveForm";
+        }
     }
 
     // URL에 테이블 명 뒤에 들어오는 값은 PK or UK
